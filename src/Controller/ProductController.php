9ac4e36bfae5daf3a1d76c1435c31874667ad3a7
Virgile <67310241+Holokazh@users.noreply.github.com>
2022-02-28 03:10:39 +0100

    Cleaning and add show
    Suppression des images non utilisées, affichage des produits, show d'un produit, possibilité de filtrer les produits

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,11 +2,14 @@
 
 namespace App\Controller;
 
+use App\Classe\Search;
 use App\Entity\Product;
+use App\Form\SearchType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class ProductController extends AbstractController
 {
@@ -16,16 +19,44 @@ class ProductController extends AbstractController
     {
         $this->entityManager = $entityManager;
     }
-    
+
     /**
      * @Route("/nos-produits", name="products")
      */
-    public function index(): Response
+    public function index(Request $request)
     {
+
         $products = $this->entityManager->getRepository(Product::class)->findAll();
+
+        $search = new Search();
+        $form = $this->createForm(SearchType::class, $search);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $products = $this->entityManager->getRepository(Product::class)->findWithSearch($search);
+        }
 
         return $this->render('product/index.html.twig', [
             'products' => $products,
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/produit/{slug}", name="product")
+     */
+    public function show($slug)
+    {
+
+        $product = $this->entityManager->getRepository(Product::class)->findOneBySlug($slug);
+
+        if (!$product) {
+            return $this->redirectToRoute('products');
+        }
+
+        return $this->render('product/show.html.twig', [
+            'product' => $product,
         ]);
     }
 }
