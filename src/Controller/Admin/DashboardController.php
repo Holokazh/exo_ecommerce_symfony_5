a26daee3ea845a41469738d3a9173e7dc02398d5
Virgile <67310241+Holokazh@users.noreply.github.com>
2022-03-22 07:12:41 +0100

    Ajout des entités Order et Orderdetails

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,15 +2,18 @@
 
 namespace App\Controller\Admin;
 
-use App\Entity\Carrier;
-use App\Entity\Category;
-use App\Entity\Product;
 use App\Entity\User;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
-use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use App\Entity\Order;
+use App\Entity\Carrier;
+use App\Entity\Product;
+use App\Entity\Category;
+use App\Controller\Admin\OrderCrudController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 
 class DashboardController extends AbstractDashboardController
 {
@@ -19,7 +22,10 @@ class DashboardController extends AbstractDashboardController
      */
     public function index(): Response
     {
-        return parent::index();
+        // redirect to some CRUD controller
+        $routeBuilder = $this->get(AdminUrlGenerator::class);
+
+        return $this->redirect($routeBuilder->setController(OrderCrudController::class)->generateUrl());
     }
 
     public function configureDashboard(): Dashboard
@@ -31,9 +37,10 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
-        yield MenuItem::linkToCrud('Utilisateur', 'fas fa-user', User::class);
-        yield MenuItem::linkToCrud('Categorie', 'fas fa-list', Category::class);
-        yield MenuItem::linkToCrud('Produit', 'fas fa-tag', Product::class);
-        yield MenuItem::linkToCrud('Transporteur', 'fas fa-truck', Carrier::class);
+        yield MenuItem::linkToCrud('Utilisateur(s)', 'fas fa-user', User::class);
+        yield MenuItem::linkToCrud('Commande(s)', 'fas fa-shopping-cart', Order::class);
+        yield MenuItem::linkToCrud('Categorie(s)', 'fas fa-list', Category::class);
+        yield MenuItem::linkToCrud('Produit(s)', 'fas fa-tag', Product::class);
+        yield MenuItem::linkToCrud('Transporteur(s)', 'fas fa-truck', Carrier::class);
     }
 }
